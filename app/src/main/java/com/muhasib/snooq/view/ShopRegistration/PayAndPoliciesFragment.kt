33f20bdaad3514101c03805046b9038e4093e983
Muhasif Hafiz <muhasibhafeez91@gmail.com>
2025-01-22 03:12:43 +0530

    Shop Registration viewPager2 Added, minor fixes

---
+++ app/src/main/java/com/muhasib/snooq/view/ShopRegistration/PayAndPoliciesFragment.kt
@@ -0,0 +1,23 @@
+package com.muhasib.snooq.view.ShopRegistration
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muhasib.snooq.R
+
+class PayAndPoliciesFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pay_and_policies, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/muhasib/snooq/view/ShopRegistration/ShopRegistrationActivity.kt
@@ -0,0 +1,89 @@
+package com.muhasib.snooq.view.ShopRegistration
+
+import BaseActivity
+import android.app.Fragment
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.muhasib.snooq.R
+import com.shuhart.stepview.StepView
+
+class ShopRegistrationActivity : BaseActivity() {
+
+    private lateinit var stepView: StepView
+    private lateinit var viewPager: ViewPager2
+    private lateinit var nextButton: Button
+    private lateinit var backButton: Button
+    private var currentStep = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_shop_registration)
+
+
+        stepView = findViewById(R.id.stepview)
+        viewPager = findViewById(R.id.viewPager)
+        nextButton = findViewById(R.id.nextButton)
+        backButton = findViewById(R.id.backButton)
+
+        stepView.go(currentStep, true)
+
+        val fragments = listOf(
+            BasicInformationFragment(),
+            LocationFragment(),
+            ShopMediaFragment(),
+            PayAndPoliciesFragment()
+
+
+        )
+
+        viewPager.adapter=object : FragmentStateAdapter(this){
+            override fun getItemCount() = fragments.size
+            override fun createFragment(position: Int) = fragments[position]
+        }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                stepView.go(position,true)
+                updateButtons(position, fragments.size)
+            }
+        })
+
+        nextButton.setOnClickListener {
+            val currentItem = viewPager.currentItem
+            if (currentItem < fragments.size - 1) {
+                viewPager.currentItem = currentItem + 1
+            } else {
+
+                Toast.makeText(this@ShopRegistrationActivity, " Shop Registered", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        backButton.setOnClickListener {
+
+            val  currentItem = viewPager.currentItem
+
+            if(currentItem>0){
+                viewPager.currentItem=currentItem-1
+            }
+        }
+
+        updateButtons(0, fragments.size)
+    }
+
+    private fun updateButtons(position: Int, total_steps: Int) {
+
+        backButton.visibility = if (position == 0) View.GONE else View.VISIBLE
+        nextButton.text = if (position == total_steps - 1) "Submit" else "Next"
+    }
+
+}
